f298a79839c29812ae6a496385bbd89c83a380f5
Vilmokas <vilmutizzz@gmail.com>
2026-01-30 22:19:54 +0200

    feat<masks>: add sprite change on new mask

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,6 +13,7 @@ public class Player : MonoBehaviour
 
     InputAction move;
     InputAction jump;
+    InputAction interact;
     Rigidbody2D body;
     int jumpCount;
     [SerializeField]
@@ -20,6 +21,11 @@ public class Player : MonoBehaviour
 
     [SerializeField]
     TYPES currentType;
+    [SerializeField]
+    GameObject interactButton;
+
+    [SerializeField]
+    GameObject[] sprites;
 
     void Start()
     {
@@ -32,15 +38,18 @@ public class Player : MonoBehaviour
     {
         move = actions.FindAction("Move");
         jump = actions.FindAction("Jump");
+        interact = actions.FindAction("Interact");
 
         move.Enable();
         jump.Enable();
+        interact.Enable();
     }
 
     void OnDisable()
     {
         move.Disable();
         jump.Disable();
+        interact.Disable();
     }
 
     void Update()
@@ -60,10 +69,28 @@ public class Player : MonoBehaviour
     {
         jumpCount = maxJumpCount;
 
+
         if (collision.gameObject.TryGetComponent(out Mask mask))
+        {
+            interactButton.SetActive(true);
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.TryGetComponent(out Mask mask))
+        {
+            interactButton.SetActive(false);
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.TryGetComponent(out Mask mask) && interact.WasPressedThisFrame())
         {
             ChangeMask(mask.type);
             collision.gameObject.SetActive(false);
+            interactButton.SetActive(false);
         }
     }
 
@@ -72,6 +99,29 @@ public class Player : MonoBehaviour
         if (currentType != newMask)
         {
             currentType = newMask;
+            switch (currentType)
+            {
+                case TYPES.basic:
+                    sprites[0].SetActive(true);
+                    sprites[1].SetActive(false);
+                    sprites[2].SetActive(false);
+                    break;
+                case TYPES.heavy:
+                    sprites[0].SetActive(false);
+                    sprites[1].SetActive(true);
+                    sprites[2].SetActive(false);
+                    break;
+                case TYPES.ranged:
+                    sprites[0].SetActive(false);
+                    sprites[1].SetActive(false);
+                    sprites[2].SetActive(true);
+                    break;
+                default:
+                    sprites[0].SetActive(true);
+                    sprites[1].SetActive(false);
+                    sprites[2].SetActive(false);
+                    break;
+            }
         }
     }
 }
