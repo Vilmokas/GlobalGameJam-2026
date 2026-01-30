82af80af202dae8afd51b74fb2c541b3fca22bce
Vilmokas <vilmutizzz@gmail.com>
2026-01-30 21:35:12 +0200

    feat<player>: add basic movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,60 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField]
+    InputActionAsset actions;
+    [SerializeField]
+    float moveSpeed;
+    [SerializeField]
+    float jumpForce;
+
+    InputAction move;
+    InputAction jump;
+    Rigidbody2D body;
+    int jumpCount;
+    [SerializeField]
+    int maxJumpCount;
+
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
+        jumpCount = maxJumpCount;
     }
 
-    // Update is called once per frame
+    void OnEnable()
+    {
+        move = actions.FindAction("Move");
+        jump = actions.FindAction("Jump");
+
+        move.Enable();
+        jump.Enable();
+    }
+
+    void OnDisable()
+    {
+        move.Disable();
+        jump.Disable();
+    }
+
     void Update()
     {
-        
+        Vector2 moveInput = move.ReadValue<Vector2>();
+
+        if (jump.WasPressedThisFrame() && jumpCount > 0)
+        {
+            body.AddForceY(jumpForce);
+            jumpCount--;
+        }
+
+        transform.Translate(
+            new Vector3(moveInput.x, 0, moveInput.y) * moveSpeed * Time.deltaTime
+        );
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        jumpCount = maxJumpCount;
     }
 }
